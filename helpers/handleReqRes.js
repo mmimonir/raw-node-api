5a56e705875e87d6a1a567247b4fd5e251fc06ff
mmimonir <mmimonir.ctg@gmail.com>
2020-11-17 01:28:07 +0600

    api-scaffolding

---
+++ helpers/handleReqRes.js
@@ -0,0 +1,57 @@
+const url = require("url");
+const { StringDecoder } = require("string_decoder");
+const routes = require("../routes");
+const {
+  notFoundHandler,
+} = require("../handlers/routeHandlers/notFoundHandler");
+
+const handler = {};
+
+handler.handleReqRes = (req, res) => {
+  // request handling
+  const parsedUrl = url.parse(req.url, true);
+  const path = parsedUrl.pathname;
+  const trimmedPath = path.replace(/^\/+|\/+$/g, "");
+  const method = req.method.toLowerCase();
+  const queryStringObject = parsedUrl.query;
+  const headersObject = req.headers;
+
+  const requestProperties = {
+    parsedUrl,
+    path,
+    trimmedPath,
+    method,
+    queryStringObject,
+    headersObject,
+  };
+
+  const decoder = new StringDecoder("utf-8");
+  let realData = "";
+
+  const chosenHandler = routes[trimmedPath]
+    ? routes[trimmedPath]
+    : notFoundHandler;
+
+  chosenHandler(requestProperties, (statusCode, payload) => {
+    statusCode = typeof statusCode === "number" ? statusCode : 500;
+    payload = typeof payload === "object" ? payload : {};
+
+    const payloadString = JSON.stringify(payload);
+
+    res.writeHead(statusCode);
+    res.end(payloadString);
+  });
+
+  req.on("data", (buffer) => {
+    realData += decoder.write(buffer);
+  });
+
+  req.on("end", () => {
+    realData += decoder.end();
+    console.log(realData);
+    // response handle
+    // res.end("Hello Students");
+  });
+};
+
+module.exports = handler;
---
+++ index.js
@@ -0,0 +1,25 @@
+// dependencies
+const http = require("http");
+
+const { handleReqRes } = require("./helpers/handleReqRes");
+
+// app object - module scaffolding
+const app = {};
+
+// configuration
+app.config = {
+  port: 3000,
+};
+
+// handle request response
+app.handleReqRes = handleReqRes;
+// create server
+app.createServer = () => {
+  const server = http.createServer(app.handleReqRes);
+  server.listen(app.config.port, () => {
+    console.log(`listening to port ${app.config.port}`);
+  });
+};
+
+// start the server
+app.createServer();
